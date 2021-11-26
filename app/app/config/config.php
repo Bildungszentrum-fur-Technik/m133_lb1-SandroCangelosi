ffba5ea06fea941f94dde7a3cacf3cfc4c4791db
sandrocangelosi <21q7237@bztf.ch>
2021-11-26 19:34:07 +0100

    am testen

--- app/app/config/config.php
+++ app/app/config/config.php
@@ -1,5 +1,12 @@
 <?php
 
+// DB-Params (die Werte sind im Docker-Compose definiert)
+define('DB_HOST', 'mysql');
+define('DB_USER', 'mensaUser');
+define('DB_PASS', 'userpass');
+define('DB_NAME', 'mensa');
+
+
 // Unsere APP-Root 
 define('APPROOT', dirname(dirname(__FILE__)));
 
--- mysql/initscripts/myexample.sql
+++ mysql/initscripts/myexample.sql
@@ -1,2 +1,4 @@
 CREATE TABLE mytable (myfield VARCHAR(20));
 INSERT INTO mytable VALUES ('Hello'), ('Dolly');
+CREATE TABLE usereintritt (myfield VARCHAR(20));
+INSERT INTO usereintritt VALUES ('Hello'), ('Dolly');
