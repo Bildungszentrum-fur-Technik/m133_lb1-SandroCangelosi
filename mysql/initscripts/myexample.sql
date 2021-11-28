1ad9d8f5af3013c1b7b94d40fc75580ca119025c
sandrocangelosi <21q7237@bztf.ch>
2021-11-28 16:06:01 +0100

    +

--- mysql/initscripts/myexample.sql
+++ mysql/initscripts/myexample.sql
@@ -30,4 +30,3 @@ VALUES ('Gustav', NULL , 'Oper', 'KV', '2021.11.27', true, true, true, true, tru
 ('Leonard', NULL , 'Geiger', 'Lehrer', '1970.01.20', true, true, true, true, true, 'Maus und Tastatur wird von dem Vorgaenger uebernommen' , true, true, true, true, true, true, 'Alles nach Checkliste erledigt', false, '2'),
 ('Sandro', 'Fritz', 'Cangelosi', 'IT', '2021.12.30', true, true, true, true, true, 'Bitte schnell erledigen', true, true, true, true, true, true, 'Alles korrekt erledigt', true, '3'),
 ('Christoph', NULL , 'Mueller', 'IT', '1991.06.01', false, false, false, false, false, 'Wichtig' , false, false, false, false, false, false, 'Bitte loeschen, Schreibfehler', false, '4');
-
