8be7c7ec8629c5626621725afa19ba500bc625d8
sandrocangelosi <21q7237@bztf.ch>
2021-11-28 10:57:28 +0100

    Testdaten

--- mysql/initscripts/myexample.sql
+++ mysql/initscripts/myexample.sql
@@ -10,9 +10,46 @@ CREATE TABLE usereintritt (
     neuestelefon boolean default false,
     winuser boolean default false,
     sapuser boolean default false,
-    bemerkungenhr VARCHAR(255)
+    bemerkungenhr VARCHAR(255),
+    checkneuerlaptop boolean default false,
+    checkneueshandy boolean default false,
+    checkneuestelefon boolean default false,
+    checkwinuser boolean default false,
+    checksapuser boolean default false,
+    checkdrucker boolean default false,
+    bemerkungenit VARCHAR(255),
+    alleserledigt boolean default false,
+    eintrittstatus tinyint(1)
 );
 
-INSERT INTO usereintritt(vorname, mittelname, nachname, jobtitel, eintrittdatum, neuerlaptop, neueshandy, neuestelefon, winuser, sapuser, bemerkungenhr) 
-VALUES ('Sandro', 'Fritz', 'Cangelosi', 'IT', '12.12.2021', true, true, true, true, true, 'Bitte schnell erledigen');
 
+INSERT INTO usereintritt(vorname, mittelname, nachname, jobtitel, eintrittdatum, neuerlaptop, neueshandy, neuestelefon, winuser, sapuser, bemerkungenhr, 
+checkneuerlaptop, checkneueshandy, checkneuestelefon, checkwinuser, checksapuser, checkdrucker, bemerkungenit, alleserledigt, eintrittstatus) 
+VALUES ('Gustav', NULL , 'Oper', 'KV', '2021.11.27', true, true, true, true, true, 'Arbeitsplatz neben Gustav' , false, false, false, false, false, false, NULL, false, '1');
+
+INSERT INTO usereintritt(vorname, mittelname, nachname, jobtitel, eintrittdatum, neuerlaptop, neueshandy, neuestelefon, winuser, sapuser, bemerkungenhr, 
+checkneuerlaptop, checkneueshandy, checkneuestelefon, checkwinuser, checksapuser, checkdrucker, bemerkungenit, alleserledigt, eintrittstatus) 
+VALUES ('Leonard', NULL , 'Geiger', 'Lehrer', '1970.01.20', true, true, true, true, true, 'Maus und Tastatur wird von dem Vorgänger übernommen' , true, true, true, true, true, true, 'Alles nach Checkliste erledigt', false, '2');
+
+INSERT INTO usereintritt(vorname, mittelname, nachname, jobtitel, eintrittdatum, neuerlaptop, neueshandy, neuestelefon, winuser, sapuser, bemerkungenhr, 
+checkneuerlaptop, checkneueshandy, checkneuestelefon, checkwinuser, checksapuser, checkdrucker, bemerkungenit, alleserledigt, eintrittstatus) 
+VALUES ('Sandro', 'Fritz', 'Cangelosi', 'IT', '2021.12.30', true, true, true, true, true, 'Bitte schnell erledigen', true, true, true, true, true, true, 'Alles korrekt erledigt', true, '3');
+
+INSERT INTO usereintritt(vorname, mittelname, nachname, jobtitel, eintrittdatum, neuerlaptop, neueshandy, neuestelefon, winuser, sapuser, bemerkungenhr, 
+checkneuerlaptop, checkneueshandy, checkneuestelefon, checkwinuser, checksapuser, checkdrucker, bemerkungenit, alleserledigt, eintrittstatus) 
+VALUES ('Christoph', NULL , 'Mueller', 'IT', '1991.06.01', false, false, false, false, false, NULL , false, false, false, false, false, false, 'Bitte loeschen, Schreibfehler', false, '4');
+
+
+
+/*
+ '' => false, 
+ '' => false, 
+ '' => false, 
+ '' => false, 
+ '' => false, 
+ '' => false, 
+ '' => '', 
+ '' => false,
+  'status' => '2'],
+
+*/
